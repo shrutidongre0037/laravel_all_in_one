7bbeaa3e9884d8a0779017b4db2ac54b2f0c031f
Shruti Dongre <shrutidongre55@gmail.com>
2025-07-25 15:07:43 +0530

    made changes to view file

--- resources/views/departments/create.blade.php
+++ resources/views/departments/create.blade.php
@@ -9,4 +9,14 @@
             ])
         </form>
     </div>
+    @if ($errors->any())
+    <div class="bg-red-100 text-red-800 p-3 mb-4 rounded">
+        <strong>There were some errors with your input:</strong>
+        <ul class="mt-2 list-disc list-inside">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 </x-app-layout>
